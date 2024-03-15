81e9c7bb542c24f628b8fa611e06506f705c57d9
Teo Lefebvre <teolefebvre@gmail.com>
2024-03-16 00:24:02 +0100

    ajout notifications par mail + uniformisation du code en anglais (sauf les commentaires) + refactoring de la structure du fichier reservations.json et du fichier .env.template

--- README.md
+++ README.md
@@ -3,20 +3,17 @@
 Pour réserver automatiquement un terrain de tennis à Bourg-la-Reine.
 A utiliser avec un cron job pour le lancer tous les jours à 7h30.
 
+## Utilisation
+
+Il faut d'abord créer un fichier `.env` sur la base du fichier `.env.template` et remplir les informations. 
+
+Le mode _DEBUG_ est activé quand la variable d'environnement `DEBUG` vaut 1. Dans ce cas là, les informations et les erreurs sont affichées en console plutôt, sinon elles sont enregistrés dans les fichiers `app.log` et `error.log`.
+
+Pour l'envoie de notifications par mail, le programme utilise _Gmail_, il faut donc créer un nouveau projet sur _Google Cloud Platform_ qui aura accès à notre compte perso pour envoyer des mails. Suivre [ce tuto](https://www.freecodecamp.org/news/use-nodemailer-to-send-emails-from-your-node-js-server/).
+
 Il faut lancer le script avec la commande : `npm run start` ou `npm start` qui permet d'inclure le fichier `.env`.
 
 ### TODO
 
-- Metre des logs pour prévenir quand la réservation n'a pas fonctionné
-- Trouver un moyen de modifier facilement en ligne le fichier de réservation (makefile qui pull de github avant d'exécuter le script ?)
-
-### Etape du programme
-
 - Se lance tous les jours à 7h31 (cron job)
-- Regarde dans fichier les réservations à faire (comment les noter ? il faut terrain date horaire et mail du partenaire de jeu)
-- Compare la date des réservations à la date d'aujourd'hui
-- Si il y en a une dans 3j lance le programme de réservation
-- Va au tableau de réservation
-- Avance de 3 jours
-- Sélectionne le bon terrain à la bonne heure (fonction pour générer l'id)
-- Réserve celui ci avec William (attention au terrain 1, il y a une étape en plus pour répondre si on veut la caméra Mojjo ou pas)
+- Trouver un moyen de modifier facilement en ligne le fichier de réservation (makefile qui pull de github avant d'exécuter le script ?)
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const puppeteer = require("puppeteer")
 const reservations = require("./reservations.json")
 const fs = require("fs/promises")
+const nodemailer = require("nodemailer")
 
-const TERRAINS = {
+const DEBUG = process.env.DEBUG === "1" // 0 pour la prod
+const COURTS = {
   "int": [24, 25, 26, 27, 28],
   "ext": [33, 31, 32] // dans l'ordre de la numérotation des terrains du club
 }
@@ -10,33 +12,43 @@ const TYPE = ["INFO", "DEBUG", "WARNING"]
 const FILES = ["app.log", "error.log"]
 
 async function log(file, type, message) {
-  let date = new Date()
-  let content = `${date.toISOString()} - ${TYPE[type]}: ${message}\n`
-  await fs.writeFile(FILES[file], content, {flag: 'a+'})
-}
-
-function verifie_reservation() {
-  const today = new Date()
-  for (let reservation of reservations) {
-    log(0, 0, "Lancement du programme.")
-    let [day, month, year] = reservation.date.split("/")
-    let date = new Date(year, month-1, day)
-    let diff = Math.ceil((date-today)/(1000*3600*24))
-    if (diff===3) {
-      log(0, 0, "Réservation trouvée.")
-      book(reservation)
-    }
-  }
-}
-
-function compute_selector(heure, i, terrain) {
-  /* selector du i-ème 1/4 d'heure du terrain choisi */
-  if (heure < 10) {
-    return `#\\3${heure} _${i*15}_${terrain}`
+  if (DEBUG) {
+    console.log(message)
   } else {
-    let [heure1, heure2] = String(heure).split("")
-    return `#\\3${heure1} ${heure2}_${i*15}_${terrain}`
+    let date = new Date()
+    let content = `${date.toISOString()} - ${TYPE[type]}: ${message}\n`
+    await fs.writeFile(FILES[file], content, {flag: 'a+'})
   }
+}
+
+function notification(mail, subject, message) {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_ADDRESS,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+  })
+  
+  let mailOptions = {
+    from: process.env.MAIL_ADDRESS,
+    to: mail,
+    subject: subject,
+    text: message
+  }
+  
+  transporter.sendMail(mailOptions, function(err, data) {
+    if (err) {
+      log(0, 1, `Le mail à ${mail} n'a pas été envoyé.`)
+      log(1, 2, err)
+    } else {
+      log(0, 0, `Le mail à ${mail} a bien été envoyé.`)
+    }
+  })
 }
 
 async function click_on_selector(page, selector) {
@@ -44,114 +56,13 @@ async function click_on_selector(page, selector) {
   await btn.click()
 }
 
-async function book(reservation) {
-  const browser = await puppeteer.launch({headless: false})
-  try {
-    const page = await browser.newPage()
-    const default_timeout = 10 // in seconds
-    page.setDefaultTimeout(default_timeout*1000)
-
-    try {
-      await page.goto("https://www.tennisblr.com/")
-      await click_on_selector(page, "#menu-item-629") // menu réservation
-    } catch (error) {
-      log(0, 2, "La page n'a pas chargée.")
-      log(1, 2, error)
-    }
-
-    const username_input_sel = ".bloc > p:nth-child(1) > input"
-    await page.waitForSelector(username_input_sel)
-    await page.focus(username_input_sel)
-    await page.keyboard.type(process.env.USERNAME)
-
-    const password_input_sel = ".bloc > p:nth-child(2) > input"
-    await page.waitForSelector(password_input_sel)
-    await page.focus(password_input_sel)
-    await page.keyboard.type(process.env.PASSWORD)
-
-    await click_on_selector(page, ".bloc > p:nth-child(3) > button") // bouton se connecter
-    await click_on_selector(page, "button.ui-button:nth-child(2)") // accéder au tableau de réservation
-
-    for (let i = 0; i < 3; i++)
-      await click_on_selector(page, "#btn_plus") // avancer d'1 jour
-
-    let booked = false
-    for (let terrain of TERRAINS[reservation.terrain]) {
-      let free = true
-      for (let i = 0; i < 4; i++) {
-        let selector = compute_selector(reservation.heure, i, terrain)
-        try {
-          await page.waitForSelector(selector, {visible: true})
-        } catch (error) {
-          free = false
-          break
-        }
-      }
-      if (!free) {
-        log(0, 0, "Pas de terrains disponibles.")
-      }
-      else {
-        let selector = compute_selector(reservation.heure, 0, terrain)
-        const rect = await page.evaluate((selector) => {
-          const element = document.querySelector(selector)
-          if (!element) return null
-          const { x, y } = element.getBoundingClientRect()
-          return { x, y }
-        }, selector)
-        await page.mouse.click(rect.x, rect.y, { clickCount: 2, delay: 100 })
-        
-        if (terrain==24) { // premier terrain donc il y a la popup pour demander si on veut utiliser la caméra Mojo
-          await click_on_selector(page, "button.bouton:nth-child(12)") // non
-          await click_on_selector(page, "button.bouton:nth-child(8)") // confirmation ne pas utiliser mojo
-        }
-
-        await click_on_selector(page, "#CHAMP_TYPE_1-button") // choisir partenaire
-        await click_on_selector(page, "#CHAMP_TYPE_1-menu > li:nth-child(2)") // désigner un partenaire
-        
-        try {
-          let [lastname, firstname] = reservation.partenaire.split(" ")
-          let partner_input_sel = ".ui-autocomplete-input"
-          await page.waitForSelector(partner_input_sel)
-          await page.focus(partner_input_sel)
-          await page.keyboard.type(lastname)
-
-          await delay(1) // attend que la liste s'affiche
-          let count = await page.evaluate((partenaire) => {
-            let ul = document.getElementById("ui-id-1")
-            let propositions = ul.children[0].children
-            let i = 0
-            for (let proposition of propositions) {
-              i++
-              if (proposition.innerText == partenaire)
-                break
-            }
-            return i
-          }, reservation.partenaire)
-
-          for (let i = 0; i < count; i++)
-            page.keyboard.press("ArrowDown") // descend pour sélectionner le bon nom
-          page.keyboard.press("Enter") // sélectionne le nom
-        } catch (error) {
-          log(0, 0, `Le partenaire ${reservation.partenaire} n'a pas été trouvé.`)
-          log(1, 2, error)
-        }
-
-        await click_on_selector(page, "button.ui-button:nth-child(6)") // valider la réservation
-
-        booked = true
-        break
-      }
-    }
-    if (booked)
-      log(0, 0, "Terrain réservé.")
-    else
-      log(0, 0, "Aucun terrain n'a été réservé.")
-
-  } catch (error) {
-    log(0, 1, "Une erreur s'est produite")
-    log(1, 2, error)
-  } finally {
-    await browser.close()
+function compute_selector(hour, i, court) {
+  /* selector du i-ème 1/4 d'heure du terrain choisi */
+  if (hour < 10) {
+    return `#\\3${hour} _${i*15}_${court}`
+  } else {
+    let [hour1, hour2] = String(hour).split("")
+    return `#\\3${hour1} ${hour2}_${i*15}_${court}`
   }
 }
 
@@ -162,4 +73,140 @@ function delay(time) {
   })
 }
 
-verifie_reservation()
+async function book(reservation) {
+  let booked = false
+  const browser = await puppeteer.launch({headless: !DEBUG})
+  try {
+    const page = await browser.newPage()
+    page.setDefaultTimeout(10_000) // en milisecondes
+
+    try {
+      await page.goto("https://www.tennisblr.com/")
+      await click_on_selector(page, "#menu-item-629") // menu réservation
+    } catch (error) {
+      log(0, 1, "La page n'a pas chargée.")
+      log(1, 2, error)
+    }
+
+    const username_input_sel = ".bloc > p:nth-child(1) > input"
+    await page.waitForSelector(username_input_sel)
+    await page.focus(username_input_sel)
+    await page.keyboard.type(process.env.TENNIS_USERNAME)
+
+    const password_input_sel = ".bloc > p:nth-child(2) > input"
+    await page.waitForSelector(password_input_sel)
+    await page.focus(password_input_sel)
+    await page.keyboard.type(process.env.TENNIS_PASSWORD)
+
+    await click_on_selector(page, ".bloc > p:nth-child(3) > button") // bouton se connecter
+    await click_on_selector(page, "button.ui-button:nth-child(2)") // accéder au tableau de réservation
+
+    for (let i = 0; i < 3; i++)
+      await click_on_selector(page, "#btn_plus") // avancer d'1 jour
+
+    page.setDefaultTimeout(2_000) // en milisecondes
+    for (let court of COURTS[reservation.court]) {
+      let free = true
+      for (let i = 0; i < 4; i++) {
+        let selector = compute_selector(reservation.hour, i, court)
+        try {
+          await page.waitForSelector(selector, {visible: true})
+        } catch (error) {
+          free = false
+          break
+        }
+      }
+      if (!free) {
+        log(0, 0, `Terrain ${court} indisponible.`)
+      }
+      else {
+        let selector = compute_selector(reservation.hour, 0, court)
+        const rect = await page.evaluate((selector) => {
+          const element = document.querySelector(selector)
+          if (!element) return null
+          const { x, y } = element.getBoundingClientRect()
+          return { x, y }
+        }, selector)
+        await page.mouse.click(rect.x, rect.y, { clickCount: 2, delay: 100 })
+        
+        if (court==24) { // premier terrain donc il y a la popup pour demander si on veut utiliser la caméra Mojo
+          await click_on_selector(page, "button.bouton:nth-child(12)") // non
+          await click_on_selector(page, "button.bouton:nth-child(8)") // confirmation
+        }
+
+        await click_on_selector(page, "#CHAMP_TYPE_1-button") // choisir partenaire
+        await click_on_selector(page, "#CHAMP_TYPE_1-menu > li:nth-child(2)") // désigner un partenaire
+        
+        try {
+          let name = `${reservation.partner.lastname} ${reservation.partner.firstname}`
+          let partner_input_sel = ".ui-autocomplete-input"
+          await page.waitForSelector(partner_input_sel)
+          await page.focus(partner_input_sel)
+          await page.keyboard.type(reservation.partner.lastname)
+
+          await delay(1) // attend que la liste s'affiche
+          let count = await page.evaluate((name) => {
+            let ul = document.getElementById("ui-id-1")
+            let propositions = ul.children[0].children
+            let i = 0
+            for (let proposition of propositions) {
+              i++
+              if (proposition.innerText == name)
+                break
+            }
+            return i
+          }, name)
+
+          for (let i = 0; i < count; i++)
+            page.keyboard.press("ArrowDown") // descend pour sélectionner le bon nom
+          page.keyboard.press("Enter") // sélectionne le nom
+
+        } catch (error) {
+          log(0, 1, `Le partenaire ${reservation.partner.firstname} ${reservation.partner.lastname} n'a pas été trouvé.`)
+          log(1, 2, error)
+        }
+
+        await click_on_selector(page, "button.ui-button:nth-child(6)") // valider la réservation
+        booked = true
+        break
+      }
+    }
+  } catch (error) {
+    log(0, 1, "Une erreur s'est produite.")
+    log(1, 2, error)
+  } finally {
+    await browser.close()
+  }
+
+  if (booked) {
+    log(0, 0, "Terrain réservé.")
+  } else {
+    let message = `La réservation pour le ${reservation.date} à ${reservation.hour}h a échoué.\nIl faut réserver manuellement.`
+    log(0, 0, message)
+    let subject = "TennisBot - échec de la réservation"
+    notification(process.env.MAIL_ADDRESS, subject, message)
+    if (!DEBUG)
+      notification(reservation.partner.mail, subject, message)
+  }
+}
+
+function check_reservation() {
+  let today = new Date()
+  let booked = false
+  for (let reservation of reservations) {
+    log(0, 0, "Lancement du programme.")
+    let [day, month, year] = reservation.date.split("/")
+    let date = new Date(year, month-1, day)
+    let diff = Math.ceil((date-today)/(1000*3600*24))
+    if (diff===3) {
+      log(0, 0, `Réservation pour le ${reservation.date} à ${reservation.hour}h trouvée.`)
+      book(reservation)
+      booked = true
+    }
+  }
+  if (!booked)
+    log(0, 0, "Aucune réservation à faire.")
+}
+
+console.log(`DEBUG: ${DEBUG}`)
+check_reservation()
